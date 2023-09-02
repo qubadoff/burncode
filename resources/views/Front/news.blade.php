c0df2a176a675bf73989d8efe6bfadf4a4180e41
qubadoff <siyaset007@gmail.com>
2023-09-03 03:06:02 +0400

    Pagination fixed for news section

--- resources/views/Front/news.blade.php
+++ resources/views/Front/news.blade.php
@@ -71,6 +71,8 @@
                             No Data !
                         @endforelse
                     </div>
+                    <br/>
+                    <br/>
                     {{ $news->links() }}
                 </div>
 
