ba01af1f84ce91e146b738e885a83839c2922b4f
qubadoff <admin@burncode.az>
2023-12-12 14:09:22 +0400

    IM

--- resources/views/Front/singleProjects.blade.php
+++ resources/views/Front/singleProjects.blade.php
@@ -20,9 +20,6 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
-                    <div class="project-thumb">
-                        <img src="{{ url('/') }}/storage/{{ $singleProject->image }}" alt="img">
-                    </div>
                     <br/>
                     <br/>
                     <br/>
