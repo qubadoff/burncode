6dbe1aa5f3bcd46df7be15164893035d9b655d4c
qubadoff <admin@burncode.az>
2023-12-12 12:11:49 +0400

    Improvements

--- app/Filament/Resources/TeamResource.php
+++ app/Filament/Resources/TeamResource.php
@@ -59,7 +59,9 @@ class TeamResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
