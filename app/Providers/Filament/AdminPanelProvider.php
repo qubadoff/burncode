785ea9ee38c5922535f5d077f5f67410ad31196a
qubadoff <admin@burncode.org>
2024-10-03 04:33:43 +0400

    serv

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->sidebarWidth(100)
             ->maxContentWidth(MaxWidth::Full)
             ->login()
             ->colors([
