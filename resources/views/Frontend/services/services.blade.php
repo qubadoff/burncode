7bc642fb5c4d523133006282f5b31ae3e92c7709
qubadoff <admin@burncode.org>
2025-07-10 15:43:51 +0400

    Version

--- resources/views/Frontend/services/services.blade.php
+++ resources/views/Frontend/services/services.blade.php
@@ -3,36 +3,32 @@
 @section('title', "Our services")
 
 @section('content')
-    @forelse($services as $service)
         <!-- Our Services Section Start -->
         <div class="our-services page-service">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4 col-md-6">
-                        <!-- Service Item Start -->
-                        <div class="service-item wow fadeInUp" data-wow-delay="0.25s">
-                            <div class="service-content">
-                                <div class="service-content-title">
-                                    <h2>{{ $service->name }}</h2>
-                                    <a href="{{ route("singleService", ["slug" => $service->slug]) }}"><img src="{{ asset('') }}assets2/images/arrow.svg" alt=""></a>
+                    @forelse($services as $service)
+                        <div class="col-lg-4 col-md-6">
+                            <!-- Service Item Start -->
+                            <div class="service-item wow fadeInUp" data-wow-delay="0.25s">
+                                <div class="service-content">
+                                    <div class="service-content-title">
+                                        <h2>{{ $service->name }}</h2>
+                                        <a href="{{ route("singleService", ["slug" => $service->slug]) }}"><img src="{{ asset('') }}assets2/images/arrow.svg" alt=""></a>
+                                    </div>
+                                    <p>
+                                        {{ $service->description }}
+                                    </p>
                                 </div>
-                                <p>
-                                    {{ $service->description }}
-                                </p>
                             </div>
-                            <div class="service-image">
-                                <figure class="image-anime">
-                                    <img src="{{ url('') }}/storage/{{ $service->image }}" alt="">
-                                </figure>
-                            </div>
+                            <!-- Service Item End -->
                         </div>
-                        <!-- Service Item End -->
-                    </div>
+                    @empty
+                        No Data !
+                    @endforelse
+
                 </div>
             </div>
         </div>
         <!-- Our Services Section End -->
-    @empty
-        No Data !
-    @endforelse
 @endsection
