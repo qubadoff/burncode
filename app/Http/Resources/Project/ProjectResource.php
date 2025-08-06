7c6ce62ac23c65133082be042d610d286fc3916c
qubadoff <admin@burncode.org>
2025-08-06 15:47:19 +0400

    s

--- app/Http/Resources/Project/ProjectResource.php
+++ app/Http/Resources/Project/ProjectResource.php
@@ -14,6 +14,15 @@ class ProjectResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'body' => $this->body,
+            'image' => url('/') . '/storage/' . $this->image,
+            'project_link' => $this->project_link,
+            'client_info' => $this->client_info,
+            'slug' => $this->slug,
+        ];
     }
 }
