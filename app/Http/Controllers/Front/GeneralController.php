37ad7cc33e77763a59d13f4922585e900ce6d984
qubadoff <admin@burncode.org>
2025-08-20 19:35:38 +0400

    cmd

--- app/Http/Controllers/Front/GeneralController.php
+++ app/Http/Controllers/Front/GeneralController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Contact\SendMessageRequest;
+use App\Mail\ContactMessageReceived;
+use App\Models\ContactMessages;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\View\View;
 
 class GeneralController extends Controller
 {
+
     public function comingSoon(): View
     {
         return \view('comingSoon');
+    }
+    public function contactSend(SendMessageRequest $request): string
+    {
+        ContactMessages::query()->create($request->validated());
+
+        Mail::to('info@burncode.org')->send(new ContactMessageReceived($request->name, $request->surname, $request->email, $request->phone, $request->body));
+
+        return back()->with('success', 'Your message has been received. One of our team members will get in touch with you shortly. Thank you!');
     }
 }
