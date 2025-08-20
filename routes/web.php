b32c6c7bf2442e81c0a18a4e64cc44a9eed0475c
qubadoff <admin@burncode.org>
2025-08-20 19:27:21 +0400

    cmd

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ Route::get('/', function (){ return redirect(app()->getLocale()); });
 
 Route::prefix('{locale}')->where(['locale' => '[a-zA-Z]{2}'])->middleware('setLocale')->group(function (){
     Route::get('/', [GeneralController::class, 'comingSoon'])->name('comingSoon');
+
+    Route::post('/contact-send', [GeneralController::class, 'contactSend'])->name('contactSend')->middleware('throttle:3,1');
 });
