f36792f31c79aa395c0a8ddcffa8c0d9aa4ef8c3
qubadoff <admin@burncode.org>
2025-08-20 19:21:36 +0400

    cmd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Front\Auth\AuthController;
 use App\Http\Controllers\Front\GeneralController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,30 +7,4 @@ Route::get('/', function (){ return redirect(app()->getLocale()); });
 
 Route::prefix('{locale}')->where(['locale' => '[a-zA-Z]{2}'])->middleware('setLocale')->group(function (){
     Route::get('/', [GeneralController::class, 'comingSoon'])->name('comingSoon');
-//    Route::get('/', [GeneralController::class, 'index'])->name('index');
-
-    Route::get('/services', [GeneralController::class, 'services'])->name('services');
-    Route::get('/services/{slug}', [GeneralController::class, 'singleService'])->name('singleService');
-
-    Route::get('/projects', [GeneralController::class, 'projects'])->name('projects');
-    Route::get('/projects/{slug}', [GeneralController::class, 'singleProjects'])->name('singleProject');
-    Route::get('/project-category/{slug}', [GeneralController::class, 'singleProjectCat'])->name('singleProjectCat');
-
-    Route::get('/products', [GeneralController::class, 'products'])->name('products');
-    Route::get('/products/{slug}', [GeneralController::class, 'singleProduct'])->name('singleProduct');
-
-    Route::get('/news', [GeneralController::class, 'news'])->name('news');
-    Route::get('/news/{slug}', [GeneralController::class, 'singleNews'])->name('singleNews');
-    Route::get('/category/{slug}', [GeneralController::class, 'singlecat'])->name('singlecat');
-
-    Route::get('/team', [GeneralController::class, 'team'])->name('team');
-    Route::get('/team/{slug}', [GeneralController::class, 'teamSingle'])->name('teamSingle');
-
-    Route::get('/contact', [GeneralController::class, 'contact'])->name('contact');
-    Route::post('/contact-send', [GeneralController::class, 'contactSend'])->name('contactSend')->middleware('throttle:3,1');
-    Route::post('/subscribe-send', [GeneralController::class, 'subscribeSend'])->name('subscribeSend')->middleware('throttle:3,1');
-
-    Route::get('/faq', [GeneralController::class, 'faq'])->name('faq');
-
-    Route::get("/login", [AuthController::class, 'login'])->name("login");
 });
