89df5c249b3136f7c69d23236dba01dbb4778b6c
qubadoff <admin@burncode.org>
2025-08-21 15:49:01 +0400

    ss

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
-use App\Http\Controllers\Front\GeneralController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (){ return redirect(app()->getLocale()); });
-
-Route::prefix('{locale}')->where(['locale' => '[a-zA-Z]{2}'])->middleware('setLocale')->group(function (){
-    Route::get('/', [GeneralController::class, 'comingSoon'])->name('comingSoon');
-
-    Route::post('/contact-send', [GeneralController::class, 'contactSend'])->name('contactSend')->middleware('throttle:3,1');
-});
+Route::get('/', function (){ return redirect('/admin'); });
